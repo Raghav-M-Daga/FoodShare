6cfd0a26257463b303038c05145053c8b24a1750
Raghav M Daga <raghavdaga2007@gmail.com>
2025-06-09 22:59:28 -0700

    fix

--- foodshare-web/app/map/page.tsx
+++ foodshare-web/app/map/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import MapPageClient from './MapPageClient';
 
 export default function MapPage() {
-  return <MapPageClient />;
+  return (
+    <Suspense fallback={<div>Loading map...</div>}>
+      <MapPageClient />
+    </Suspense>
+  );
 }
